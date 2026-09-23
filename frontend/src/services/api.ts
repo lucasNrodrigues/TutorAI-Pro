407b9867b90a6cc817fc1531157b9de99eeabcfc
lucasNrodrigues <luxuryh293@gmail.com>
2026-09-23 11:51:43 -0300

    style: modernizando a barra de XP

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,17 +1,20 @@
-import axios from 'axios';
-import { MetricaProgresso } from '../types';
+import axios from "axios";
+import { MetricaProgresso } from "../types";
 
 export const api = axios.create({
-  // Colocando o link direto para ignorar o erro da Vercel
-  baseURL: 'https://tutorai-backend-km0b.onrender.com',
+  baseURL: "https://tutorai-backend-km0b.onrender.com",
 });
 
-export async function getMetricasAluno(alunoId: number): Promise<MetricaProgresso[]> {
+export async function getMetricasAluno(
+  alunoId: number
+): Promise<MetricaProgresso[]> {
   try {
-    // Usando a instância 'api' que configuramos acima
     const resposta = await api.get(`/alunos/${alunoId}/metricas`);
+
     return resposta.data;
   } catch (error) {
-    throw new Error('Falha ao buscar as métricas do aluno');
+    console.error("Erro ao buscar métricas:", error);
+
+    throw new Error("Falha ao buscar as métricas do aluno");
   }
 }
